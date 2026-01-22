bfa4bce57bdb67ad69808c9a05873418954d899a
CodingArbor <53602274+CodingArbor@users.noreply.github.com>
2026-01-22 10:09:16 +0800

    修复CORS漏洞

--- server/http_header.go
+++ server/http_header.go
@@ -7,17 +7,18 @@ import (
 )
 
 func (c *Server) CrossOrigin(w http.ResponseWriter, r *http.Request) {
-	if r.Header.Get("Origin") != "" {
-		w.Header().Set("Access-Control-Allow-Origin", r.Header.Get("Origin"))
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-	} else {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		r.Header.Set("Origin", "*")
-	}
-	w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, X-Requested-By, If-Modified-Since, X-File-Name, X-File-Type, Cache-Control, Origin")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Expose-Headers", "Authorization")
-	//https://blog.csdn.net/yanzisu_congcong/article/details/80552155
+	w.Header().Set("Access-Control-Allow-Headers",
+		"Authorization, Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, X-Requested-By, If-Modified-Since, X-File-Name, X-File-Type, Cache-Control, Origin")
+
+	// ⚠️ 不允许凭证
+	// w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+	if r.Method == http.MethodOptions {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
 }
 
 func (c *Server) SetDownloadHeader(w http.ResponseWriter, r *http.Request, isSmall bool) {
